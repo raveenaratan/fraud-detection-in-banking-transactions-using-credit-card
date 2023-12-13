134ca1f9067d31234f966b505c44bbcce1d964f5
raveenaratan96@gmail.com <raveenaratan96@gmail.com>
2023-12-13 16:18:15 +0000

    Created ServerModule1 and Home; Edited standard-page.html, theme/parameters.yaml and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,7 +1,7 @@
 from ._anvil_designer import Form1Template
 
 from anvil import *
-
+import anvil.server
 
 
 class Form1(Form1Template):
@@ -11,7 +11,21 @@ class Form1(Form1Template):
     # Set Form properties and Data Bindings.
 
     self.init_components(**properties)
+    
+
 
 
 
     # Any code you write here will run before the form opens.
+
+  def text_box_1_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
+  def form_show(self, **event_args):
+    """This method is called when the form is shown on the page"""
+    pass
