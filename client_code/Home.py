fe2d7567ee5c55495f3932f5e5f2bfc7c3d87343
raveenaratan96@gmail.com <raveenaratan96@gmail.com>
2023-12-14 15:52:36 +0000

    Edited Home

--- client_code/Home.py
+++ client_code/Home.py
@@ -1,11 +1,29 @@
 import anvil.server
-# This is a module.
-# You can define variables and functions here, and use them from any form. For example, in a top-level form:
-#
-#    from . import Module1
-#
-#    Module1.say_hello()
-#
 
-def say_hello():
-  print("Hello, world")
+# Get live fraud detection data
+@anvil.server.callable
+def get_fraud_data():
+  # Retrieve and return fraud stats
+  return {
+    "transactions" : 10000,
+    "fraud_transactions" : 203,
+    "fraud_rate" : "2.03%"    
+  }
+
+# Client side Form code
+class HomeForm(HomeFormTemplate):
+
+  def __init__(self, **properties):
+    # Set init properties from server function
+    self.init_components(**properties)
+
+    # Attach server function to button
+    self.get_data_button.set_event_handler("click", self.get_data)
+
+  # Retrieve fraud data  
+  def get_data(self, **event_args):
+    self.report_panel.visible = False
+    fraud_data = anvil.server.call('get_fraud_data')
+    
+    self.transactions_label.text = "Total Transactions: " + str(fraud_data["transactions"])
+    self.fraud_transactions_label.text ="Fraud Transactions: " + str(fra
