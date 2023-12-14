fd0a674e05f33cbdf2d2e854e41e58f2d7302b69
raveenaratan96@gmail.com <raveenaratan96@gmail.com>
2023-12-14 17:35:00 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -29,6 +29,18 @@ class Form1(Form1Template):
     """This method is called when the button is clicked"""
     pass
 
+  def button_1_click(self, **event_args):
+    # Display a popup that says 'You clicked the button'
+    alert("You clicked the button")
+
   def form_show(self, **event_args):
     """This method is called when the form is shown on the page"""
     pass
+
+    def button_1_click(self, **event_args):
+    # Set 'name' to the text in the 'name_box'
+    Name = self.name_box.text
+    # Set 'email' to the text in the 'email_box'
+    E-mail = self.email_box.text
+    # Set 'feedback' to the text in the 'feedback_box'
+    Phone = self.Phone_box.text
